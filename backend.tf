011be172843fd9ecad08355eba2d240ff448e892
Shubhangi-Sirsat <163672965+Shubhangi-Sirsat@users.noreply.github.com>
2025-02-16 14:50:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket= "mydev-project-terraform-batch-26-ss-1502"
+    bucket= "mydev-project-terraform-batch-26-ss-rk"
     key = "main"
     region="us-east-1"
     dynamodb_table = "my-dynamodb-table-ss"
